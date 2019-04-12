a84de1727196ec0b062bbbe77b58d97d838d9734
Robin Herbots <robin.herbots@gmail.com>
2019-04-12 14:49:09 +0200

    first version

--- README.md
+++ README.md
@@ -17,73 +17,27 @@ Once the plugin has been installed, it may be enabled inside your Gruntfile with
 grunt.loadNpmTasks('grunt-cideploy');
 ```
 
-## The "cideploy" task
+## The "ci_deploy" task
 
 ### Overview
 In your project's Gruntfile, add a section named `cideploy` to the data object passed into `grunt.initConfig()`.
 
 ```js
 grunt.initConfig({
-  cideploy: {
-    options: {
-      // Task-specific options go here.
-    },
-    your_target: {
-      // Target-specific file lists and/or options go here.
-    },
-  },
-});
+  ci_deploy: {
+      target: {
+        options: {
+             before: function (grunt, options) { },
+             msbuild: [],
+             buildtasks: ["nugetrestore", "msbuild"],
+             after: function (grunt, options) { },
+             origin: "",
+             username: grunt.option("username"),
+             password: grunt.option("password"),
+             Project_x0020_NameId: 1,
+             Application_x0020_NameId: 12,
+             Title: 'Title'
+        }
+      }
+  });
 ```
-
-### Options
-
-#### options.separator
-Type: `String`
-Default value: `',  '`
-
-A string value that is used to do something with whatever.
-
-#### options.punctuation
-Type: `String`
-Default value: `'.'`
-
-A string value that is used to do something else with whatever else.
-
-### Usage Examples
-
-#### Default Options
-In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
-
-```js
-grunt.initConfig({
-  cideploy: {
-    options: {},
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
-```
-
-#### Custom Options
-In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
-
-```js
-grunt.initConfig({
-  cideploy: {
-    options: {
-      separator: ': ',
-      punctuation: ' !!!',
-    },
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
-```
-
-## Contributing
-In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
-
-## Release History
-_(Nothing yet)_
--- tasks/cideploy.js
+++ tasks/cideploy.js
@@ -14,12 +14,22 @@ var _ = require("lodash");
 module.exports = function (grunt) {
     grunt.registerMultiTask('ci_deploy', "Gitlab ci deploy", function () {
         var options = this.options({
-            username: grunt.option("username"),
-            password: grunt.option("password"),
-            before: function(grunt, options){},
+            before: function (grunt, options) {
+            },
             msbuild: [],
             buildtasks: ["nugetrestore", "msbuild"],
-            after: function(grunt, options){}
+            after: function (grunt, options) {
+            },
+            origin: "",
+            username: grunt.option("username"),
+            password: grunt.option("password"),
+            Project_x0020_NameId: 1,
+            Application_x0020_NameId: 12,
+            Title: 'Title',
+        });
+
+        _.merge(grunt.config.data, {
+            pkg: grunt.file.readJSON("package.json")
         });
 
         function startDeploy(tag) {
@@ -41,15 +51,16 @@ module.exports = function (grunt) {
             grunt.option("totag", tag);
 
             options.before(grunt, options);
-            for(var i = 0; i< options.buildtasks.length; i++) {
-                //generate
-                // grunt.config('msbuild.RIMService.options.buildParameters.DeployOnBuild', true);
-                // grunt.config('msbuild.RIMGui.options.buildParameters.DeployOnBuild', true);
-                // grunt.config('msbuild.RIMRegistration.options.buildParameters.DeployOnBuild', true);
+
+            if (options.buildtasks.length > 0) {
+                _.merge(grunt.config.data, {
+                    msbuild: options.msbuild
+                });
+                console.log(JSON.stringify(grunt.config("msbuild")));
             }
-            console.log(JSON.stringify(grunt.config("msbuild")));
-            grunt.task.run(["clean", "nugetrestore", "msbuild"]);
-            options.before(grunt, options);
+            grunt.task.run(options.buildtasks);
+
+            options.after(grunt, options);
 
             _.merge(grunt.config.data, {
                 gitcheckout: {
@@ -70,7 +81,7 @@ module.exports = function (grunt) {
                             options: {
                                 seturl: {
                                     name: "origin",
-                                    url: "https://gitlab.com/reynaers/webteam/usermanagement/ReynaersIdentity.git"
+                                    url: options.origin
                                 }
                             }
                         }
@@ -103,6 +114,132 @@ module.exports = function (grunt) {
                 grunt.task.run("gittag:priv");
             }
             if (grunt.option('profile') === "Production") {
+                grunt.registerTask("PSP", "Publish release info on sharepoint", function () {
+                    function generateReleaseInfo() {
+                        var prefix = ">> ",
+                            strBuild = [], logs = grunt.config("gitlog.privReleaseInfo.result");
+                        for (var lg in logs) {
+                            if (logs[lg].subject.indexOf(prefix) === 0) {
+                                strBuild.push(logs[lg].subject.substring(prefix.length));
+                            }
+                        }
+
+                        return strBuild.join("<br/>");
+                    }
+
+                    if (!grunt.option("totag")) {
+                        _.merge(grunt.config.data, {
+                            gitdescribe: {
+                                privLatest: {
+                                    options: {
+                                        abbrev: 0,
+                                        callback: function (result) {
+                                            console.log(result);
+                                            grunt.option("totag", result);
+                                        }
+                                    }
+                                }
+                            }
+                        });
+
+                        grunt.task.run("gitdescribe:privLatest");
+                    }
+
+                    if (!grunt.option("fromtag")) {
+                        _.merge(grunt.config.data, {
+                            gitdescribe: {
+                                privPrevious: {
+                                    options: {
+                                        abbrev: 0,
+                                        "commit-ish": grunt.option("totag") + "^",
+                                        callback: function (result) {
+                                            console.log(result);
+                                            grunt.option("fromtag", result);
+                                        }
+                                    }
+                                }
+                            }
+                        });
+
+                        grunt.task.run("gitdescribe:privPrevious");
+                    }
+
+                    grunt.registerTask("PSP_spawn", function () {
+                        _.merge(grunt.config.data, {
+                            gitcheckout: {
+                                priv: {
+                                    options: {
+                                        branch: `tags/${grunt.option("totag")}`,
+                                        // create: true,
+                                        overwrite: true,
+                                        force: true
+                                    }
+                                }
+                            }
+                        });
+                        grunt.task.run("gitcheckout:priv");
+                        _.merge(grunt.config.data, {
+                            gitlog: {
+                                privReleaseInfo: {
+                                    options: {
+                                        prop: 'gitlog.privReleaseInfo.result',
+                                        from: grunt.option("fromtag"),
+                                        to: grunt.option("totag"),
+                                        // pretty: "%cn - %s",
+                                        number: 25,
+                                        callback: function () {
+                                            _.merge(grunt.config.data, {
+                                                sharepoint_list: {
+                                                    priv: {
+                                                        options: {
+                                                            username: options.username,
+                                                            password: options.password,
+                                                            ntlm_domain: 'reynaers',
+                                                            list: 'ApplicationReleaseInfo',
+                                                            base: 'https://together.reynaers.com/depict',
+                                                            listitem: {
+                                                                __metadata: {
+                                                                    type: 'SP.Data.ApplicationReleaseInfoListItem'
+                                                                },
+                                                                Project_x0020_NameId: options.Project_x0020_NameId,
+                                                                Application_x0020_NameId: options.Application_x0020_NameId,
+                                                                Title: options.Title,
+                                                                Release_x0020_Date: grunt.option("date") || grunt.template.today('yyyy/mm/dd'),
+                                                                VersionNumber: '<%= pkg.version %>',
+                                                                ReleaseNotes: generateReleaseInfo()
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            });
+
+                                            console.log(JSON.stringify(grunt.config("sharepoint_list")));
+                                            grunt.task.run("sharepoint_list");
+
+                                            _.merge(grunt.config.data, {
+                                                gitcheckout: {
+                                                    privMaster: {
+                                                        options: {
+                                                            branch: 'master',
+                                                            force: true
+                                                        }
+                                                    }
+                                                }
+                                            });
+                                            grunt.task.run("gitcheckout:privMaster");
+                                        }
+                                    }
+                                }
+                            }
+                        });
+
+                        grunt.task.run(["gitlog:privReleaseInfo"]);
+                    });
+
+                    grunt.task.run("PSP_spawn");
+                });
+
+
                 grunt.task.run("PSP");
             }
         }
@@ -141,129 +278,5 @@ module.exports = function (grunt) {
 
     });
 
-    grunt.registerTask("PSP", "Publish release info on sharepoint", function () {
-        function generateReleaseInfo() {
-            var prefix = ">> ",
-                strBuild = [], logs = grunt.config("gitlog.privReleaseInfo.result");
-            for (var lg in logs) {
-                if (logs[lg].subject.indexOf(prefix) === 0) {
-                    strBuild.push(logs[lg].subject.substring(prefix.length));
-                }
-            }
-
-            return strBuild.join("<br/>");
-        }
-
-        if (!grunt.option("totag")) {
-            _.merge(grunt.config.data, {
-                gitdescribe: {
-                    privLatest: {
-                        options: {
-                            abbrev: 0,
-                            callback: function (result) {
-                                console.log(result);
-                                grunt.option("totag", result);
-                            }
-                        }
-                    }
-                }
-            });
-
-            grunt.task.run("gitdescribe:privLatest");
-        }
-
-        if (!grunt.option("fromtag")) {
-            _.merge(grunt.config.data, {
-                gitdescribe: {
-                    privPrevious: {
-                        options: {
-                            abbrev: 0,
-                            "commit-ish": grunt.option("totag") + "^",
-                            callback: function (result) {
-                                console.log(result);
-                                grunt.option("fromtag", result);
-                            }
-                        }
-                    }
-                }
-            });
-
-            grunt.task.run("gitdescribe:privPrevious");
-        }
-
-        grunt.registerTask("PSP_spawn", function () {
-            _.merge(grunt.config.data, {
-                gitcheckout: {
-                    priv: {
-                        options: {
-                            branch: `tags/${grunt.option("totag")}`,
-                            // create: true,
-                            overwrite: true,
-                            force: true
-                        }
-                    }
-                }
-            });
-            grunt.task.run("gitcheckout:priv");
-            _.merge(grunt.config.data, {
-                gitlog: {
-                    privReleaseInfo: {
-                        options: {
-                            prop: 'gitlog.privReleaseInfo.result',
-                            from: grunt.option("fromtag"),
-                            to: grunt.option("totag"),
-                            // pretty: "%cn - %s",
-                            number: 25,
-                            callback: function () {
-                                _.merge(grunt.config.data, {
-                                    sharepoint_list: {
-                                        priv: {
-                                            options: {
-                                                username: options.username,
-                                                password: options.password,
-                                                ntlm_domain: 'reynaers',
-                                                list: 'ApplicationReleaseInfo',
-                                                base: 'https://together.reynaers.com/depict',
-                                                listitem: {
-                                                    __metadata: {
-                                                        type: 'SP.Data.ApplicationReleaseInfoListItem'
-                                                    },
-                                                    Project_x0020_NameId: 1,
-                                                    Application_x0020_NameId: 12,
-                                                    Title: 'Reynaers Identity Management',
-                                                    Release_x0020_Date: grunt.option("date") || grunt.template.today('yyyy/mm/dd'),
-                                                    VersionNumber: '<%= pkg.version %>',
-                                                    ReleaseNotes: generateReleaseInfo()
-                                                }
-                                            }
-                                        }
-                                    }
-                                });
-
-                                console.log(JSON.stringify(grunt.config("sharepoint_list")));
-                                grunt.task.run("sharepoint_list");
-
-                                _.merge(grunt.config.data, {
-                                    gitcheckout: {
-                                        privMaster: {
-                                            options: {
-                                                branch: 'master',
-                                                force: true
-                                            }
-                                        }
-                                    }
-                                });
-                                grunt.task.run("gitcheckout:privMaster");
-                            }
-                        }
-                    }
-                }
-            });
-
-            grunt.task.run(["gitlog:privReleaseInfo"]);
-        });
-
-        grunt.task.run("PSP_spawn");
-    });
 
 };
